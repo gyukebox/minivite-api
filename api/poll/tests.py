3e3b7a9c5622157c92c42e77ca3cf773b04abbc9
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-11-11 16:08:25 +0900

    데이터베이스 모델 변경, 유닛테스트 추가

--- api/poll/tests.py
+++ api/poll/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import *
+
 
 # Create your tests here.
+class CreatePollTests(TestCase):
+    """
+    Creating polls test
+    """
+    def test_is_created(self):
+        sample_poll = PollModel(title='이것은 테스트 투표')
+        selections = [SelectionModel(poll=sample_poll, body='이것은 테스트 선택지 {}'.format(i)) for i in range(5)]
+        sample_poll.save()
+        [selection.save() for selection in selections]
+
+        self.assertIsNot(PollModel.objects.get(title=sample_poll.title), None)
+
+
+class UpdatePollTests(TestCase):
+    pass
--- api/poll/views.py
+++ api/poll/views.py
@@ -3,31 +3,6 @@ from django.http import HttpResponse
 from .models import *
 
 
-# Create your views here.
-def index(request):
-    print(request.body)
-    return HttpResponse('hello')
-
-
-def poll_create(request):
-    # json 을 풀어서 리스트로 만들 수 있거든요
-    import json
-    poll_info = request.body
-
-    new_model = PollModel()
-    try:
-        new_model.title = poll_info['title']
-        for choice in poll_info['choices']:
-            new_selection = SelectionModel()
-
-    except IndexError:
-        return HttpResponse(status=400)
-
-    new_model.save()
-
-    return HttpResponse(status=200)
-
-
 def poll_update(request):
     """
     투표 결과 실시간 반영 뷰 함수
@@ -36,6 +11,8 @@ def poll_update(request):
     """
     to_update = json.loads(request.body, encoding='utf8')
     poll = PollModel.objects.get(title=to_update['pollTitle'])
-
+    selection_lists = SelectionModel.objects.filter(poll_id=poll.id)
+    for list in selection_lists:
+        pass
 
     return HttpResponse(status=200)
